45a3871b81e58b0f4f13527ad8d57c53052426b1
Anup-Gautam <84380379+Anup-Gautam@users.noreply.github.com>
2023-02-04 20:56:42 -0600

    Update learningLoops.java

--- learningLoops.java
+++ learningLoops.java
@@ -23,10 +23,56 @@ public class learningLoops{
         else{
             System.out.println("You are a child!");
         }
+        
+        
+        Scanner scanner = new Scanner(System.in);
+         String name = "";
+
+         //Executing while loop
+
+         while(name.isBlank()){
+             System.out.println("Enter your name: ");
+             name = scanner.nextLine();
+         }
+            // There is another variation do... while and this is used if we have to use the code at least once no matter the input
+//         do{
+//             System.out.println("why you no enter name?");
+//         }while(name.isBlank());
+
+        System.out.println("Hello " + name);
 
 
+         //for loop executes limited number of times
+        //for (declaration, condition, spaces){
+        // block of code }
+        for(int idx = 0; idx <= 10; idx++) {
+            System.out.println(idx);
+        }
 
+        for(int idx = 10; idx>= 0; idx--){
+            System.out.println(idx);
+        }
 
+        //nested loops
+
+        Scanner scan = new Scanner(System.in);
+        int rows;
+        int cols;
+        String symbol = "";
+
+        System.out.println("Enter num of row: ");
+        rows = scan.nextInt();
+        System.out.println("Enter num of cols: ");
+        cols = scan.nextInt();
+        System.out.println("Enter the symbol to use: ");
+        symbol = scanner.next();
+
+        for(int i = 1; i<= rows; i++) {
+            System.out.println(); // linebreak
+            for(int j = 1; j<=cols; j++){
+                System.out.print(symbol);
+            }
+        }
 
     }
 }
